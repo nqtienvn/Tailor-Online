481da0edb26e2617c83f36e691727f7018c1a60c
nqtienvn <quangtiencoder1876@gmail.com>
2025-11-04 23:47:02 +0700

    push up template customer-service for developer

---
+++ customer-service/src/main/java/com/tien/tai/presentation/rest/impl/CustomerResourceImpl.java
@@ -0,0 +1,10 @@
+package com.tien.tai.presentation.rest.impl;
+
+import com.tien.tai.presentation.rest.CustomerResource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CustomerResourceImpl implements CustomerResource {
+}
